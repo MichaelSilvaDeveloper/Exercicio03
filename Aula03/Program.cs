27dfc0be4b582c9e7a33c2fcc33dad3adb7806dc
Michael <Michael@DESKTOP-61L2M0C>
2023-10-16 19:37:28 -0300

    Adicionar arquivos de projeto.

---
+++ Aula03/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1;
+            int num2;
+            int res;
+            num1 = 10;
+            num2 = 2;
+            res = num1 * num2;
+            Console.WriteLine("Numero: {0}", num1);
+            Console.WriteLine("Numero: {0}", num2);
+            Console.WriteLine("A multipicação e: {0}", res);
+
+        }
+    }
+}
